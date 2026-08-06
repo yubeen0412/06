8022e13bf613d2b8a14aaf2d5e539c368134ad14
yubeen0412 <lyubeen07@gmail.com>
2026-08-07 00:50:40 +0900

    practice 1-1

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+void print_star()
+{
+	int i;
+	for(i=0; i<10;i++)
+	printf("*");
+}
+
 int main(void)
 {
-	int i; 
-	
-	for(i = 0; i < 10; i++ ) 
-	printf(“*”); 
-	for( i = 0; i < 10; i++ ) 
-	printf(“*”); 
-	for( i = 0; i < 10; i++ ) 
-	printf(“*”); 
+	print_star();
+	print_star();
+	print_star();
 	
 	return 0;
 }
