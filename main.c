11d2b878fb403d15f4e2c6195db6da46108d190f
yubeen0412 <lyubeen07@gmail.com>
2026-08-07 01:56:10 +0900

    practice 5

--- main.c
+++ main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 
-int square(int a) 
-{ 
-return (a *a); 
-} 
-int main() 
-{ 
-int a = 3; 
-a = square(a); 
-printf("a=%i\n",a); 
+int n, r;
+
+int main(void)
+{
+	get_interger();
+	printf("combination=%d\n", combination(n, r));
+	return 0;
+	
+}
+
+int combination (int n, int r)
+{
+	return (factorial(n) / (factorial(r)*factorial(n-r)));
+}
+
+int factorial (int num)
+{
+	int i, res=1;
+	
+	for(i=1; i<=num; i++)
+		res = res*i;
+	return res;
+}
+
+int get_interger ()
+{
+	printf("input n:");
+	scanf("%d", &n);
+	printf("input r:");
+	scanf("%d", &r);
+	return 0;
 }
